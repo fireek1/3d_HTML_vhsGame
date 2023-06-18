ac8a1ab8806e10ef92b7e3c2254f5d4adeb30f5c
fireek1 <odvdplvdplv@gmail.com>
2023-06-18 21:24:14 +0200

    0.0.2

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const cube = document.querySelector('.cube')
 const scene = document.querySelector('.scene')
 const ui = document.querySelector('.ui')
+const pyramid = document.querySelector('.pyramid')
 
 let translateZ = 0
 let translateX = 0
 
 let rotateX = 0
 let rotateY = 0
+let rotateZ = 0
 
 let prevMouseX = 0
 let prevMouseY = 0
@@ -23,64 +25,54 @@ document.onkeydown = (e) => {
     if (e.code === 'KeyW') {
         translateZ += 10
         originZ -= 10
-    }
-    if (e.code === 'KeyS') {
+    } else if (e.code === 'KeyS') {
         translateZ -= 10
         originZ += 10
-    }
-    if (e.code === 'KeyA') {
+    } else if (e.code === 'KeyA') {
         translateX += 10
         originX -= 10
-    }
-    if (e.code === 'KeyD') {
+    } else if (e.code === 'KeyD') {
         translateX -= 10
         originX += 10
-    }
-    if (e.code === 'Space') {
+    } else if (e.code === 'Space') {
         rotateX = 0
         rotateY = 0
+        rotateZ = 0
         perspective = 500
-    }
-    if (e.code === 'ArrowUp') {
+    } else if (e.code === 'ArrowUp') {
         perspective += 10
-    }
-    if (e.code === 'ArrowDown') {
+    } else if (e.code === 'ArrowDown') {
         perspective -= 10
-    }
-
-    if (e.code === 'ArrowLeft') {
+    } else if (e.code === 'ArrowLeft' && blur > 0) {
         blur -= 1
-    }
-    if (e.code === 'ArrowRight') {
+    } else if (e.code === 'ArrowRight') {
         blur += 1
     }
 
     if(translateZ >= 5000 && cover < 1) {
         cover += 0.005
     }
-
-    
-    cube.style.transform = `translate3d(${translateX}px, 0, ${translateZ}px) rotateY(${rotateX}deg) rotateX(${rotateY}deg)`
+   
+    cube.style.transform = `translate3d(${translateX}px, 0, ${translateZ}px) rotateY(${rotateX}deg) rotateX(${rotateY * Math.cos(rotateX * Math.PI / 180)}deg) rotateZ(${rotateZ * Math.sin(rotateX * Math.PI / 180)}deg)`
     cube.style.transformOrigin = `${originX}px 50% ${originZ}px`
     scene.style.perspective = `${perspective}px`
     ui.style.backgroundColor = `rgba(0, 0, 0, ${cover})`;
     ui.style.backdropFilter = `blur(${blur}px)`
 }
 
-
-
-
 document.onmousemove = (e) => {
-    const currentMouseX = e.clientX
+    const currentMouseX = e.clientX 
     const currentMouseY = e.clientY
 
-    const deltaX = currentMouseX - prevMouseX
+    const deltaX = (currentMouseX - prevMouseX)
     const deltaY = currentMouseY - prevMouseY
+    const deltaZ = (currentMouseX - prevMouseX)
 
-    rotateY += -deltaY * 0.2
+    rotateY += -deltaY * 0.2 
     rotateX += deltaX * 0.2
+    rotateZ += -deltaY * 0.2
 
-    cube.style.transform = `translate3d(${translateX}px, 0, ${translateZ}px) rotateY(${rotateX}deg) rotateX(${rotateY}deg)`
+    cube.style.transform = `translate3d(${translateX}px, 0, ${translateZ}px) rotateY(${rotateX}deg) rotateX(${rotateY * Math.cos(rotateX * Math.PI / 180)}deg) rotateZ(${rotateZ * Math.sin(rotateX * Math.PI / 180)}deg)` //Math.sin(rotateX * Math.PI / 180)
 
     prevMouseX = currentMouseX
     prevMouseY = currentMouseY
