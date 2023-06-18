bbe407d5bd862119c8956087ae5ae24d0fe7d633
fireek1 <odvdplvdplv@gmail.com>
2023-06-19 00:08:15 +0200

    0.0.3

--- scripts/script.js
+++ scripts/script.js
@@ -13,27 +13,45 @@ let rotateZ = 0
 let prevMouseX = 0
 let prevMouseY = 0
 
-let originZ = 500
-let originX = 150
+let originZ = 0
+let originX = 0
 
 let perspective = 500
 let cover = 0
 let blur = 0
 
+let dx = 0
+let dy = 0
+
+const speed = 10
+
 document.onkeydown = (e) => {
 
-    if (e.code === 'KeyW') {
-        translateZ += 10
-        originZ -= 10
-    } else if (e.code === 'KeyS') {
-        translateZ -= 10
-        originZ += 10
+    if (e.code === 'KeyS') {
+        dx = Math.sin(rotateX * Math.PI / 180)
+        dy = -Math.cos(rotateX * Math.PI / 180)
+        translateX += dx * speed;
+        translateZ += dy * speed;
+        // originZ -= translateZ
+    } else if (e.code === 'KeyW') {
+        dx = -Math.sin(rotateX * Math.PI / 180)
+        dy = Math.cos(rotateX * Math.PI / 180)
+        translateX += dx * speed;
+        translateZ += dy * speed;
+        // originZ += translateZ
     } else if (e.code === 'KeyA') {
-        translateX += 10
-        originX -= 10
+        dx = Math.sin(rotateX * Math.PI / 180 + Math.PI / 2)
+        dy = -Math.cos(rotateX * Math.PI / 180 + Math.PI / 2)
+        translateX += dx * speed;
+        translateZ += dy * speed;
+        // originX -= translateX
+        
     } else if (e.code === 'KeyD') {
-        translateX -= 10
-        originX += 10
+        dx = -Math.sin(rotateX * Math.PI / 180 + Math.PI / 2)
+        dy = Math.cos(rotateX * Math.PI / 180 + Math.PI / 2)
+        translateX += dx * speed;
+        translateZ += dy * speed;
+        // originX = translateX
     } else if (e.code === 'Space') {
         rotateX = 0
         rotateY = 0
@@ -49,10 +67,16 @@ document.onkeydown = (e) => {
         blur += 1
     }
 
-    if(translateZ >= 5000 && cover < 1) {
+    if (translateZ >= 5000 && cover < 1) {
         cover += 0.005
     }
-   
+
+    originX = -translateX + 150
+    originZ = -translateZ + 500
+
+    console.log(originX)
+
+
     cube.style.transform = `translate3d(${translateX}px, 0, ${translateZ}px) rotateY(${rotateX}deg) rotateX(${rotateY * Math.cos(rotateX * Math.PI / 180)}deg) rotateZ(${rotateZ * Math.sin(rotateX * Math.PI / 180)}deg)`
     cube.style.transformOrigin = `${originX}px 50% ${originZ}px`
     scene.style.perspective = `${perspective}px`
@@ -60,19 +84,26 @@ document.onkeydown = (e) => {
     ui.style.backdropFilter = `blur(${blur}px)`
 }
 
+document.onkeyup = (e) => {
+    if (e.code === 'KeyW' || e.code === 'KeyA' || e.code === 'KeyS' || e.code === 'KeyD') {
+        dx = 0
+        dy = 0
+    }
+}
+
 document.onmousemove = (e) => {
-    const currentMouseX = e.clientX 
+    const currentMouseX = e.clientX
     const currentMouseY = e.clientY
 
     const deltaX = (currentMouseX - prevMouseX)
     const deltaY = currentMouseY - prevMouseY
     const deltaZ = (currentMouseX - prevMouseX)
 
-    rotateY += -deltaY * 0.2 
+    rotateY += -deltaY * 0.2
     rotateX += deltaX * 0.2
     rotateZ += -deltaY * 0.2
 
-    cube.style.transform = `translate3d(${translateX}px, 0, ${translateZ}px) rotateY(${rotateX}deg) rotateX(${rotateY * Math.cos(rotateX * Math.PI / 180)}deg) rotateZ(${rotateZ * Math.sin(rotateX * Math.PI / 180)}deg)` //Math.sin(rotateX * Math.PI / 180)
+    cube.style.transform = `translate3d(${translateX}px, 0, ${translateZ}px) rotateY(${rotateX}deg) rotateX(${rotateY * Math.cos(rotateX * Math.PI / 180)}deg) rotateZ(${rotateZ * Math.sin(rotateX * Math.PI / 180)}deg)`
 
     prevMouseX = currentMouseX
     prevMouseY = currentMouseY
