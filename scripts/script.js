90adef70fca96975fa3a3f239bff6ca3ebdf91fa
fireek1 <134843809+fireek1@users.noreply.github.com>
2023-06-21 18:28:20 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -32,63 +32,6 @@ const speed = 7
 
 let enemyCount = 0
 
-// document.onkeydown = (e) => {
-//     if (e.code === 'KeyS') {
-//         dx = Math.sin(rotateX * Math.PI / 180)
-//         dy = -Math.cos(rotateX * Math.PI / 180)
-//         translateX += dx * speed
-//         translateZ += dy * speed
-//         isMoving = true
-//     } else if (e.code === 'KeyW') {
-//         dx = -Math.sin(rotateX * Math.PI / 180)
-//         dy = Math.cos(rotateX * Math.PI / 180)
-//         translateX += dx * speed
-//         translateZ += dy * speed
-//         isMoving = true
-//     } else if (e.code === 'KeyA') {
-//         dx = Math.sin(rotateX * Math.PI / 180 + Math.PI / 2)
-//         dy = -Math.cos(rotateX * Math.PI / 180 + Math.PI / 2)
-//         translateX += dx * speed
-//         translateZ += dy * speed
-//         isMoving = true
-//     } else if (e.code === 'KeyD') {
-//         dx = -Math.sin(rotateX * Math.PI / 180 + Math.PI / 2)
-//         dy = Math.cos(rotateX * Math.PI / 180 + Math.PI / 2)
-//         translateX += dx * speed
-//         translateZ += dy * speed
-//         isMoving = true
-//     } else if (e.code === 'Space') {
-//         rotateX = 0
-//         rotateY = 0
-//         rotateZ = 0
-//         perspective = 500
-//     } else if (e.code === 'ArrowUp') {
-//         perspective += 10
-//     } else if (e.code === 'ArrowDown') {
-//         perspective -= 10
-//     } else if (e.code === 'ArrowLeft' && blur > 0) {
-//         blur -= 1
-//     } else if (e.code === 'ArrowRight') {
-//         blur += 1
-//     }
-
-//     if (translateZ >= 5000 && cover < 1) {
-//         cover += 0.005
-//     }
-
-//     originX = -translateX + 150
-//     originZ = -translateZ + 500
-
-//     console.log(e.code)
-
-
-//     cube.style.transform = `translate3d(${translateX}px, 0, ${translateZ}px) rotateY(${rotateX}deg) rotateX(${rotateY * Math.cos(rotateX * Math.PI / 180)}deg) rotateZ(${rotateZ * Math.sin(rotateX * Math.PI / 180)}deg)`
-//     cube.style.transformOrigin = `${originX}px 50% ${originZ}px`
-//     scene.style.perspective = `${perspective}px`
-//     ui.style.backgroundColor = `rgba(0, 0, 0, ${cover})`;
-//     ui.style.backdropFilter = `blur(${blur}px)`
-// }
-
 document.onkeyup = (e) => {
     if (e.code === 'KeyW' || e.code === 'KeyA' || e.code === 'KeyS' || e.code === 'KeyD') {
         dx = 0
@@ -188,40 +131,9 @@ setInterval(function () {
     }
 }, 1000 / 60);
 
-// document.onmousedown = (e) => {
-//     // rotateEnemy()
-// }
-
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-
-// function rotateEnemy() {
-//     const newEnemy = enemy.cloneNode(true)
-//     document.querySelector('.cube').appendChild(newEnemy)
-//     const enemyX = random(translateX - 2000, translateX + 2000)
-//     const enemyZ = random(translateZ - 2000, translateZ + 2000)
-
-//     deltaX = translateX - enemyX
-//     deltaZ = translateZ - enemyZ
-
-//     const angle = Math.atan2(deltaZ, deltaX)
-//     const degrees = angle * (180 / Math.PI);
-//     // newEnemy.classList.remove('eye')
-//     newEnemy.id = `enemy-${enemyCount}`
-
-//     newEnemy.style.transform = `translate3d(${enemyX}px, 100px, ${enemyZ}px) rotateZ(${degrees}deg)`
-//     newEnemy.style.scale = '0.1'
-//     enemyCount++
-//     console.log(enemyZ)
-
-// }
-
-// setInterval(() => {
-//     enemy
-// })
-
 
 function spawnEnemy() {
     const newEnemy = enemy.cloneNode(true);
